b0282b0e3d90cc9e1c59e2816a75aead3204000a
Xilltro <93945221+999dyl@users.noreply.github.com>
2021-11-11 22:50:18 +0000

    Update index.js

--- events/index.js
+++ events/index.js
@@ -1,1 +1,22 @@
 const = require("discord.js")
+const = require("typescript.js")
+const = require("python.py")
+
+const => prefix = [
+  file = os.fetch("config.json");
+  else {
+    set = prefix("/")
+    }
+]
+
+const => status(bot.status{
+  client.status = [
+    .type = "Playing";
+    .description = "/help";
+    ]
+});
+
+const => newMessage(if message.content === 'index'{
+  client.On(process.env.token())
+  client.bot.run(token=".env")
+});
