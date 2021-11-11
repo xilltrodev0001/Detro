4ffd89d7155b5a6dc75224c77a44c425d33273ea
Xilltro <93945221+999dyl@users.noreply.github.com>
2021-11-11 23:11:24 +0000

    Update mute.js

--- commands/javascript/mute.js
+++ commands/javascript/mute.js
@@ -1,2 +1,43 @@
 const = require('discord.py')
 const = require('discord.js')
+
+const => new.message(if message.content === 'mute'{
+  const => embed(new.command) {
+    .setTitle("Muted Successfully")
+    .setDescription("{format.user.name.muted} Was Muted By {author.mention()} for {reason}")
+    .setFooter("Muted")
+    .setColor(RANDOM)
+    .setFooterIcon(images/footericon.png)
+    .setAuthor(name="Muted", icon="images/authoricon.png")
+    };
+    class muted_role(self.role()) {
+      if server.guild === role("muted", "Muted") not_exists:
+      await error.send("Sorry but there is so mute role")
+      else {
+        if server.guild === role("Muted", "muted") exists:
+        await member.mute for time()=!true()
+        }
+      }
+    if user == not_mentioned = {
+      await message.send("Sorry but you didnt mention a user")
+      error()
+      }
+      if user == no_time = {
+        await error.send("Sorry but you didn't provide a time")
+        error()
+        }
+        if user == no_reason = {
+          await message.send("Sorry but there is so reason")
+          error()
+          }
+          else {
+            if reason = true;
+            if time = true;
+            if mention = true;
+             Bot.AddRole == for user.mentioned(());
+             role.add == "Muted";
+             if time == out()===!bot.unmute.user(())
+             await message.send(content="That user was unmuted automatically their time for the mute has ran out")
+             unmute()
+            };
+});
