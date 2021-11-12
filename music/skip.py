344eb546bde6427687c33fd1ca65497247256dc4
Xilltro <93945221+xilltrodev0001@users.noreply.github.com>
2021-11-12 13:10:07 +0000

    Create skip.py

---
+++ music/skip.py
@@ -0,0 +1,25 @@
+import discord
+import ytdl
+import os
+import api
+from ytdl import *
+from ytdl import Api
+from discord.ext import modules
+from discord.ext import commands
+from ytdl import ytdl.exports
+
+@bot.command()
+async def skip(ytld.skip : fetch.current_song, votes=None):
+  if user = skips:
+    await bot.message(f"{usersInVC} People Need to vote in-order to skip):
+      else:
+        if user = in = VC.(alone==:discord.Utils):
+        self.song.skip()
+        
+        def skip(self.skip):
+          self.fetch.song()
+          self.skip.song = True()
+     
+     skip = ytdl.skip.song.url = True():
+       vc.play.new.song():
+         skipSong()
