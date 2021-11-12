36ab1c79beeddfe02162c6a1bfb979736e8489ca
Xilltro <93945221+999dyl@users.noreply.github.com>
2021-11-12 00:52:45 +0000

    Create data.js

---
+++ configuration/data.js
@@ -0,0 +1,26 @@
+const = require("components.js")
+const = require("commands.js")
+const = require("fileLink.js"())
+
+if bot === 'data.errors' {
+  class errors(self.system.errors()) {
+    write.error(if bot === 'offline')
+    server.guild.message {
+      // in channel === 'updates', 'news', 'announcements', 'changelog', 'general', 'chat',
+      message.content == ("The bot is offline, this message it sent by the system, the bot will be back up")
+      await message.send(==)
+      }
+      class edit_bot_status = [
+        NEW_STATUS == type("Playing"), description("The bot is offline")
+        await change()
+        ]
+        offline = false()
+    }
+}
+
+class data(self.data, system) {
+  data = data;
+  while true:
+   // return data
+   exec == files()/true()
+}
