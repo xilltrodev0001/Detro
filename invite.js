fbef49d782d5109ffb6d525d71b7e1b512ddfd79
Xilltro <93945221+xilltrodev0001@users.noreply.github.com>
2021-11-12 13:26:55 +0000

    Create invite.js

---
+++ invite.js
@@ -0,0 +1,21 @@
+const = require("discord.js")
+const = require("typescript.js")
+const = require("commands.js")
+
+prefix == set("/")
+
+const => embed.Message({
+  new = message.Embed()
+  if message.content === 'invite' {
+     .setTitle("Invite Detro")
+     .setDescription("[invite me](https://top.gg/bot-invite/detrobot) - Invite Now)
+     .setFooter("Invite Me Now..")
+     .setColor(RANDOM)
+     };
+     await message.send(embed)
+     await user.dm() {
+       dm.message.content(text="Message Sent Successfully").embed.(type=Title);
+       .setMessageDmColor(RANDOM)
+       };
+       message.send();
+});
