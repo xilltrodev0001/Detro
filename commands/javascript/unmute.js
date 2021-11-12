3ecf7ad38e99d89aefce42f72997e126f0b7f0be
Xilltro <93945221+999dyl@users.noreply.github.com>
2021-11-12 01:00:53 +0000

    Update unmute.js

--- commands/javascript/unmute.js
+++ commands/javascript/unmute.js
@@ -1,2 +1,23 @@
 const = require("discord.js")
-const = require("database/")
+const = require("configuration/...config.json")
+
+const => content(if message.content === 'unmute'{
+  new => embed = ({
+    .setTitle("Unmuted")
+    .setAuthor(name="Successfully Unmuted", icon="images/author.png")
+    .setDescription("{user.Mentioned()} Was Unmuted")
+    .setFooter("Unmuted • If it was a mistake do /mute again")
+    .removeRole("Muted", "muted", if usermentioned==is(true).remove)
+    });
+    self.remove(role==='Muted', 'muted')
+      {
+        system.remove.role()
+        await update()
+      };
+    if user == unmuted = by(time):
+    await message.say("Unmuted Because The Time Ran Out")
+    else {
+      if user == unmuted = by(author):
+      await message.say("Unmuted by {format.author.mention()}")
+      };
+});
