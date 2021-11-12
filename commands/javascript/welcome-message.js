7a9e9ce2c745bbe83d7a97450048dd91272df2a9
Xilltro <93945221+999dyl@users.noreply.github.com>
2021-11-12 01:43:04 +0000

    Create welcome-message.js

---
+++ commands/javascript/welcome-message.js
@@ -0,0 +1,17 @@
+const = require("discord.js")
+const = require("cogs.js")
+
+const => new.message(if member === {joins.guild}) {
+   content = [
+     .setTitle = "Welcome to {guild.server.name} you are now the {memberCountOnJoined} Member Now",
+     .setThumbnail = "images/welcome.jpeg",
+     .setColor = "discord.Color_blue()"
+     ]
+     for channels = [
+       topic = "WELCOME"
+       // 'welcome', 'arrivals', 'joins', 'joins-leaves',
+       if fetched == channel()
+       send.message = true
+       ]
+   }
+   await message.send()
