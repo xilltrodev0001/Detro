fac9de1b01484d2c5214f1a5c6e1043ef63ad516
Xilltro <93945221+xilltrodev0001@users.noreply.github.com>
2021-11-12 02:02:37 +0000

    Create play.py

---
+++ music/play.py
@@ -0,0 +1,45 @@
+import ytdl
+import souncloudapi
+import spotifyapi
+import os
+import discord
+from discord.ext import commands
+from ytdl import APIs
+from soundcloudapi import *
+from spotifyapi import *
+
+@bot.command()
+async def play(ctx, *, ytdl_search : joinVoice, *, connect):
+  
+  def join(vc):
+    connect()==!True() {
+      util.connect == vc.user.in()
+      if user = not_in_vc [] > !error(), await ctx.send("Sorry but you need to be in a VC to use this command**).embedded()
+      else:
+        if user = in(VC):
+        bot.connect()
+      }
+      
+     if user = leaves.voice.channel():
+       bot.leave(())
+
+    def play(song_url, ctx, utils):
+      message = from user():
+      search_song () through.api()=True:
+        
+        add_reactions = [
+          :YouTube:
+          :Spotify:
+          :SoundCloud:
+          ]
+      
+       if found == song()/MP3__file__.play()
+       else:
+         if not_found = error("Sorry but that song wasn't food")
+
+     def keywords(self.keywords):
+       keywords = [
+         "song",
+         "https",
+         "by",
+         ]
