ca1f60cabe83d19a54074dfd5bbdb1b1bc851e63
Xilltro <93945221+999dyl@users.noreply.github.com>
2021-11-12 01:20:39 +0000

    Update ban.py

--- commands/python/ban.py
+++ commands/python/ban.py
@@ -2,3 +2,39 @@ import discord
 from discord.ext import commands
 from discord.ext import cogs
 from discord.ext import handlers
+
+@cogs.command():
+  name == 'ban'
+    guild = cog.access = guilds,
+    users = cog.access = users,
+
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx : discord.Member, *, reason=None):
+  await member.ban()
+  embed = discord.Embed(title=f"Banned From {guild.id}", description=f"Banned: {userMentioned}, Banned by:{format.ctx.authorMention()} for {reason}", discord.Color_red())
+  member.ban()
+
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def unban(ctx, discord.Member : unban.ctx.data, *, id=None):
+  
+  for guild.bans(server.id) {
+    ban.entries()
+    user.member == (name) + (#).split() {
+      collect.name()
+      collect.#()
+      }
+    }
+     
+    ban_entries == 'bans':
+      for guilds:
+        await member.unban from guild()
+
+       embed = discord.Embed(title=f"{user} was unbanned from {guildID}, by {format.ctx.authorMention()}")
+       await ctx.send(embed=embed)
+
+    module_exports = [
+      unban == 'unban.py";
+      ban == 'ban.py"
+      ]
