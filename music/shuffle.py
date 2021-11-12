265201ed3a2592da0e973bf2b97e404786c45618
Xilltro <93945221+xilltrodev0001@users.noreply.github.com>
2021-11-12 13:05:40 +0000

    Create shuffle.py

---
+++ music/shuffle.py
@@ -0,0 +1,22 @@
+import discord 
+import ytdl
+import apis
+import ost
+
+from discord.ext import commands
+from ytdl import music.commands
+from ost import ostMusic.api
+
+@bot.command()
+async def shuffle(utils : ,*, shuffle.current.song=True, : discord.VC):
+  embed = discord.embed(title="Shuffled Successfully", color=discord.Color_black())
+  embed.set_author("Shuffled By".format.authorMention())
+  
+  def shuffle(song):
+    get = utils()==>discord.Commands():
+      self.shuffle.songs_queue() 
+
+
+@clean.error
+if error = while Shuffling():
+  self.await.message.send("Sorry but there was a error")
