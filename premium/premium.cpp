f3da7f08787dc716603bceb01e8d294f398d7d26
Xilltro <93945221+999dyl@users.noreply.github.com>
2021-11-12 01:34:33 +0000

    Update premium.cpp

--- premium/premium.cpp
+++ premium/premium.cpp
@@ -1,3 +1,11 @@
 #include systems;
 #include discord;
 #include payments;
+
+methods = message.send():
+   message.content in list():
+     "PayPal: https://paypal.me/user/xilltrodev"
+     "Cash App: https://cashapp.com/give_money/user/xilltrodev"
+     "Website: https://detro.glitch.me"
+
+message.send()
